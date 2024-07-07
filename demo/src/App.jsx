82b5ea61a3f761917beb05b9d5cd5831e85ef84c
hari2u96 <hari2u96@gmail.com>
2024-07-07 22:49:59 +0530

    ff

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -6,11 +6,10 @@ function App() {
 
   return (
     
-      <h1>hello</h1>
-      <h2>This is from prasath branch</h2>
-=======
+   <>
     <BrowserRouter>
     <h1>hello</h1>
+    <h2>This is HARI Branch</h2>
     </BrowserRouter>
      
 
