4c0f339d1d1901db0dc6d83bbf2c3de0ec102a0a
hari2u96 <118653238+hari2u96@users.noreply.github.com>
2024-07-07 22:53:15 +0530

    Update App.jsx

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -6,9 +6,7 @@ function App() {
 
   return (
     
-      <h1>hello</h1>
-      <h2>This is from prasath branch</h2>
-=======
+    
     <BrowserRouter>
     <h1>hello</h1>
     <h2>This is Hari Branch</h2>
